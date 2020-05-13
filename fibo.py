7486477fa8ca0b5e1039c5370c8f99a330724acf
jkmartin5500 <jkmartin5500@gmail.com>
2020-05-13 13:03:17 -0400

    Fibonacci Numbers

---
+++ fibo.py
@@ -0,0 +1,35 @@
+import re
+
+
+def parse_data(filename):
+    # Get all numbers from the input data
+    file = open(filename, 'r')
+    contents = file.read()
+    file.close()
+    input = re.compile('\d+').findall(contents)
+    input = [int(i) for i in input]
+    # Returned the parsed input data
+    return input[0]
+
+
+def fibo(fibo_dest):
+    if fibo_dest < 2:
+        return fibo_dest
+        
+    sequence = [0, 1]
+    for i in range(fibo_dest - 1):
+        sequence.append(sequence[i] + sequence[i + 1])
+    return sequence[len(sequence) - 1]
+
+
+def main():
+    input = parse_data("sample.txt")
+    answer = fibo(input)
+    output = open("output.txt", 'w')
+    print(answer)
+    output.write(str(answer))
+    output.close()
+
+
+if __name__ == "__main__":
+    main()
