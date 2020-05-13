f691503936cb3d2e3b3269752a396dd19be32148
jkmartin5500 <jkmartin5500@gmail.com>
2020-05-13 17:54:46 -0400

    Double-Degree Array

---
+++ ddeg.py
@@ -0,0 +1,41 @@
+import re
+
+
+def parse_data(filename):
+    # Get all numbers from the input data
+    file = open(filename, 'r')
+    contents = file.read()
+    file.close()
+    contents = re.compile('-?\d+').findall(contents)
+    contents = [int(i) for i in contents]
+    # Returned the parsed input data
+    graph = {}
+    for i in range(contents[0]):
+        graph[i+1] = []
+    for i in range(contents[1]):
+        graph[contents[2 * i + 2]].append(contents[2 * i + 3])
+        graph[contents[2 * i + 3]].append(contents[2 * i + 2])
+    return graph, contents[0]
+
+
+def deg(graph, vertices):
+    degrees = [0]*vertices
+    for i in range(vertices):
+        for neighbor in graph[i+1]:
+            degrees[i] += len(graph[neighbor])
+    return degrees
+
+
+
+def main():
+    input = parse_data("rosalind_ddeg.txt")
+    answer = deg(input[0], input[1])
+    output = open("output.txt", 'w')
+    print(answer)
+    for i in range(len(answer)):
+        output.write(str(answer[i]) + ' ')
+    output.close()
+
+
+if __name__ == "__main__":
+    main()
